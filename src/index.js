a97490f785888b29a9133a753276a4bd065fe49a
openbravo <openbravo@MacBook-Pro-de-openbravo-2.local>
2017-07-18 17:41:30 +0200

    Many improvements

--- src/index.js
+++ src/index.js
@@ -6,23 +6,43 @@ import "./alberite.css";
 
 class Header extends React.Component {
   render() {
+    const tabs = [
+      { id: "status", name: "Sistema" },
+      { id: "riego", name: "Riego" }
+    ];
+    const menuItems = tabs.map(menuItem => {
+      let theClassName = "pure-menu-item";
+      theClassName +=
+        menuItem.id === this.props.currentTab ? " pure-menu-selected" : "";
+      return (
+        <li className={theClassName} key={menuItem.id}>
+          <a
+            className="pure-menu-link alberite_cursor"
+            onClick={() => {
+              this.props.setMenuTab(menuItem.id);
+            }}
+          >
+            {menuItem.name}
+          </a>
+        </li>
+      );
+    });
+    menuItems.push(
+      <li className="pure-menu-item" key="logout">
+        <a href="/logout" className="pure-menu-link alberite_cursor">
+          Salir
+        </a>
+      </li>
+    );
+
     return (
       <div className="header">
         <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed menu_shadow">
-          <a className="pure-menu-heading" href="">
+          <a className="pure-menu-heading alberite_page_name" href="">
             Villa Cautela
           </a>
           <ul className="pure-menu-list">
-            <li className="pure-menu-item pure-menu-selected">
-              <a href="#" className="pure-menu-link">
-                Riego
-              </a>
-            </li>
-            <li className="pure-menu-item">
-              <a href="/logout" className="pure-menu-link">
-                Salir
-              </a>
-            </li>
+            {menuItems}
           </ul>
         </div>
       </div>
@@ -30,35 +50,71 @@ class Header extends React.Component {
   }
 }
 
-class MyTable extends React.Component {
+class AlberiteTable extends React.Component {
+  generateTable() {
+    return true;
+  }
+  additionalRow() {
+    return null;
+  }
+  additionalColumn() {
+    return null;
+  }
+  additionalColumnHeader() {
+    return null;
+  }
   render() {
-    const theRows = this.props.rows.map((row, idx) => {
-      const theRow = row;
-      const values = this.props.headerProps.map(headerProp => {
-        let value = theRow[headerProp.prop];
-        if (headerProp.prop.toLowerCase().indexOf("date") !== -1) {
-          const date = new Date(Date.parse(value));
-          value =
-            date.toLocaleDateString() +
-            " " +
-            pad(date.getHours(), 2) +
-            ":" +
-            pad(date.getMinutes(), 2) +
-            ":" +
-            pad(date.getSeconds(), 2);
-        }
-        return (
-          <td>
-            {value}
-          </td>
-        );
-      });
-      return (
-        <tr className="pure-table-odd" key={row.id ? row.id : row.index}>
-          {values}
-        </tr>
-      );
-    });
+    let theRows;
+    if (!this.generateTable()) {
+      theRows = [];
+    } else {
+      if (this.props.rows === null) {
+        theRows = [
+          <tr className="pure-table-odd" key="loadingRows">
+            <td key="loadingRow" colSpan={this.props.headerProps.length}>
+              Cargando información...
+            </td>
+          </tr>
+        ];
+      } else {
+        theRows = this.props.rows.map((row, idx) => {
+          const theRow = row;
+          const keyValue = row.id ? row.id : row.index;
+          const values = this.props.headerProps.map(headerProp => {
+            let value = theRow[headerProp.prop];
+            if (headerProp.prop.toLowerCase().indexOf("date") !== -1) {
+              const date = new Date(Date.parse(value));
+              value =
+                date.toLocaleDateString() +
+                " " +
+                pad(date.getHours(), 2) +
+                ":" +
+                pad(date.getMinutes(), 2) +
+                ":" +
+                pad(date.getSeconds(), 2);
+            }
+            return (
+              <td key={(row.id ? row.id : row.index) + headerProp.prop}>
+                {value}
+              </td>
+            );
+          });
+          const additionalColumn = this.additionalColumn(keyValue);
+          if (additionalColumn !== null) {
+            values.push(additionalColumn);
+          }
+          return (
+            <tr className="pure-table-odd" key={keyValue}>
+              {values}
+            </tr>
+          );
+        });
+      }
+    }
+    const additionalRow = this.additionalRow();
+    if (additionalRow !== null) {
+      theRows.push(additionalRow);
+    }
     const theHeader = this.props.headerProps.map((headerProp, idx) => {
       return (
         <th key={headerProp.name}>
@@ -66,40 +122,328 @@ class MyTable extends React.Component {
         </th>
       );
     });
+    const additionalColumnHeader = this.additionalColumnHeader();
+    if (additionalColumnHeader !== null) {
+      theHeader.push(additionalColumnHeader);
+    }
     return (
-      <table className="pure-table pure-table-horizontal alberite_generic_table alberite_log_table">
-        <thead>
-          <tr key="header">
-            {theHeader}
-          </tr>
-        </thead>
-        <tbody>
-          {theRows}
-        </tbody>
-      </table>
+      <div>
+        <table className="pure-table pure-table-horizontal alberite_generic_table alberite_log_table">
+          <thead>
+            <tr key="header">
+              <th
+                colSpan={
+                  this.props.headerProps.length +
+                  (this.additionalColumnHeader() !== null ? 1 : 0)
+                }
+              >
+                {this.props.tableName}
+              </th>
+            </tr>
+            <tr key="headerProps">
+              {theHeader}
+            </tr>
+          </thead>
+          <tbody>
+            {theRows}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+class AlberiteHour extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hour: 0,
+      minute: 0,
+      second: 0
+    };
+  }
+  generateHourFromState() {
+    let hour = "'";
+    hour += pad(this.state.hour, 2);
+    hour += ":";
+    hour += pad(this.state.minute, 2);
+    hour += ":";
+    hour += pad(this.state.second, 2);
+    hour += "'";
+    return hour;
+  }
+  setHour(value) {
+    this.setState({ hour: value }, () =>
+      this.props.changeHappened(this.generateHourFromState())
+    );
+  }
+  setMinute(value) {
+    this.setState({ minute: value }, () =>
+      this.props.changeHappened(this.generateHourFromState())
+    );
+  }
+  setSecond(value) {
+    this.setState({ second: value }, () =>
+      this.props.changeHappened(this.generateHourFromState())
+    );
+  }
+  render() {
+    return (
+      <div className="alberite_hour_div">
+        <input
+          className="alberite_hour_inputs"
+          onChange={evt => {
+            this.setHour(evt.target.value);
+          }}
+        />:<input
+          className="alberite_hour_inputs"
+          onChange={evt => {
+            this.setMinute(evt.target.value);
+          }}
+        />:<input
+          className="alberite_hour_inputs"
+          onChange={evt => {
+            this.setSecond(evt.target.value);
+          }}
+        />
+      </div>
     );
   }
 }
 
-class Body extends React.Component {
+class AlberiteEditingTable extends AlberiteTable {
+  constructor(props) {
+    super(props);
+    this.state = this.generateInitialState();
+  }
+  generateInitialState() {
+    const initialState = {};
+    this.props.headerProps.forEach(headerProp => {
+      if (headerProp.possibleValues) {
+        initialState[headerProp.prop] = headerProp.possibleValues[0].value;
+      }
+    });
+    return initialState;
+  }
+  setValueInState(value) {
+    this.setState(value);
+  }
+  additionalColumnHeader() {
+    return <td key="additionalColHeader" />;
+  }
+  additionalColumn(index) {
+    return (
+      <td key="deleteButton">
+        <button
+          onClick={() => {
+            this.props.deleteActionHandler(index);
+          }}
+        >
+          Borrar
+        </button>
+      </td>
+    );
+  }
+  additionalRow() {
+    const fields = this.props.headerProps.map(headerProp => {
+      let field;
+      if (headerProp.type === "combo") {
+        const options = headerProp.possibleValues.map(possibleValue => {
+          return (
+            <option key={possibleValue.id} value={possibleValue.value}>
+              {possibleValue.name}
+            </option>
+          );
+        });
+        field = (
+          <select
+            className="alberite_combo"
+            value={this.state[headerProp.prop]}
+            onChange={evt => {
+              let value = {};
+              value[headerProp.prop] = evt.target.value;
+              this.setValueInState(value);
+            }}
+          >
+            {options}
+          </select>
+        );
+      } else if (headerProp.type === "hour") {
+        field = (
+          <AlberiteHour
+            changeHappened={theValue => {
+              let value = {};
+              value[headerProp.prop] = theValue;
+              this.setValueInState(value);
+            }}
+          />
+        );
+      } else {
+        field = <input />;
+      }
+      return (
+        <td key={headerProp.name}>
+          {field}
+        </td>
+      );
+    });
+    fields.push(
+      <td key="okButton">
+        <button
+          onClick={() => {
+            this.props.createActionHandler(this.state);
+          }}
+        >
+          Ok
+        </button>
+      </td>
+    );
+    return (
+      <tr className="pure-table-odd" key="editingRow">
+        {fields}
+      </tr>
+    );
+  }
+}
+
+class AlberiteEditingRow extends AlberiteEditingTable {
+  generateTable() {
+    return false;
+  }
+}
+
+class StatusBody extends React.Component {
   render() {
     return (
       <div className="splash-container alberite_splash_container">
-        <MyTable
+        <div className="alberite_generic_dummy_div_first" />
+        <AlberiteTable
           rows={this.props.logRows}
           headerProps={[
             { name: "Mensaje", prop: "message" },
             { name: "Fecha", prop: "messagedate" },
             { name: "IP", prop: "externalip" }
           ]}
+          tableName="Mensajes de log"
         />
-        <MyTable
-          rows={this.props.actionRows}
+        <AlberiteTable
+          rows={this.props.pastActionRows}
           headerProps={[
             { name: "Fase", prop: "phase" },
             { name: "Duración", prop: "duration" },
             { name: "Fecha", prop: "date" }
           ]}
+          tableName="Riegos pasados"
+        />
+        <AlberiteTable
+          rows={this.props.actionRows}
+          headerProps={[
+            { name: "Fase", prop: "phase" },
+            { name: "Duración", prop: "time" }
+          ]}
+          tableName="Riegos en cola"
+        />
+      </div>
+    );
+  }
+}
+
+class RiegoBody extends React.Component {
+  render() {
+    const programFields = [
+      {
+        name: "Fase",
+        prop: "phase",
+        type: "combo",
+        possibleValues: [
+          { id: 1, name: "Piscina fase 1", value: 1 },
+          { id: 2, name: "Piscina fase 2", value: 2 },
+          { id: 3, name: "Aspersores fase 1", value: 3 },
+          { id: 4, name: "Entrada+Cerezo", value: 4 },
+          { id: 5, name: "Huerta", value: 5 }
+        ]
+      },
+      {
+        name: "Duración",
+        prop: "time",
+        type: "combo",
+        possibleValues: [
+          { id: 1, name: "Pulso (10 secs)", value: 10 },
+          { id: 2, name: "30 segundos", value: 30 },
+          { id: 3, name: "1 minuto", value: 60 },
+          { id: 4, name: "2 minutos", value: 120 },
+          { id: 5, name: "3 minutos", value: 180 },
+          { id: 6, name: "4 minutos", value: 240 },
+          { id: 7, name: "5 minutos", value: 300 },
+          { id: 8, name: "10 minutos", value: 600 },
+          { id: 9, name: "15 minutos", value: 900 },
+          { id: 10, name: "20 minutos", value: 1200 }
+        ]
+      },
+      {
+        name: "Frecuencia",
+        prop: "frequency",
+        type: "combo",
+        possibleValues: [
+          { id: 1, name: "DIARIA", value: "DIARIA" },
+          { id: 2, name: "DÍAS IMPARES", value: "DÍAS IMPARES" },
+          { id: 3, name: "DÍAS PARES", value: "DÍAS PARES" }
+        ]
+      },
+      { name: "Hora", prop: "hour", type: "hour" }
+    ];
+    const directFields = [
+      {
+        name: "Fase",
+        prop: "pin",
+        type: "combo",
+        possibleValues: [
+          { id: 1, name: "Piscina fase 1", value: 1 },
+          { id: 2, name: "Piscina fase 2", value: 2 },
+          { id: 3, name: "Aspersores fase 1", value: 3 },
+          { id: 4, name: "Entrada+Cerezo", value: 4 },
+          { id: 5, name: "Huerta", value: 5 }
+        ]
+      },
+      {
+        name: "Duración",
+        prop: "duration",
+        type: "combo",
+        possibleValues: [
+          { id: 1, name: "Pulso (10 secs)", value: 10 },
+          { id: 2, name: "30 segundos", value: 30 },
+          { id: 3, name: "1 minuto", value: 60 },
+          { id: 4, name: "2 minutos", value: 120 },
+          { id: 5, name: "3 minutos", value: 180 },
+          { id: 6, name: "4 minutos", value: 240 },
+          { id: 7, name: "5 minutos", value: 300 },
+          { id: 8, name: "10 minutos", value: 600 },
+          { id: 9, name: "15 minutos", value: 900 },
+          { id: 10, name: "20 minutos", value: 1200 }
+        ]
+      }
+    ];
+    return (
+      <div className="splash-container alberite_splash_container">
+        <div className="alberite_generic_dummy_div_first" />
+        <AlberiteEditingTable
+          rows={this.props.programmedActionRows}
+          headerProps={programFields}
+          tableName="Riegos programados"
+          createActionHandler={this.props.onProgrammedAction}
+          deleteActionHandler={this.props.onDeleteProgrammedAction}
+        />
+        <AlberiteTable
+          rows={this.props.actionRows}
+          headerProps={[
+            { name: "Fase", prop: "phase" },
+            { name: "Duración", prop: "time" }
+          ]}
+          tableName="Riegos en cola"
+        />
+        <AlberiteEditingRow
+          headerProps={directFields}
+          tableName="Riego directo"
+          createActionHandler={this.props.onDirectAction}
         />
       </div>
     );
@@ -110,10 +454,80 @@ class MainLayout extends React.Component {
   constructor() {
     super();
     this.state = {
-      logRows: [],
-      actionRows: []
+      logRows: null,
+      actionRows: null,
+      pastActionRows: null,
+      programmedActionRows: null,
+      currentTab: "status"
     };
+
+    this.loadData();
+    setInterval(() => this.loadData(), 5000);
+  }
+  deleteProgrammedValue(index) {
     var me = this;
+    fetch(
+      "http://villacautela.com/cancelProgrammedAction?programmedAction=" +
+        index,
+      {
+        mode: "cors",
+        method: "get"
+      }
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        //TODO: MESSAGE!!!
+        me.loadData();
+      })
+      .catch(function(err) {
+        console.error("error while reading loginfo: ", err);
+      });
+  }
+  modifyDirectValues(values) {
+    var me = this;
+    fetch(
+      "http://villacautela.com/startSystemAction?" + buildGETRequest(values),
+      {
+        mode: "cors",
+        method: "get"
+      }
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        //TODO: MESSAGE!!!
+        me.loadData();
+      })
+      .catch(function(err) {
+        console.error("error while reading loginfo: ", err);
+      });
+  }
+  modifyProgrammedValues(values) {
+    var me = this;
+    fetch(
+      "http://villacautela.com/insertProgrammedAction?" +
+        buildGETRequest(values),
+      {
+        mode: "cors",
+        method: "get"
+      }
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        //TODO: MESSAGE!!!
+        me.loadData();
+      })
+      .catch(function(err) {
+        console.error("error while reading loginfo: ", err);
+      });
+  }
+  loadData() {
+    const me = this;
     fetch("http://villacautela.com/loginfo", { mode: "cors", method: "get" })
       .then(function(response) {
         return response.json();
@@ -131,19 +545,72 @@ class MainLayout extends React.Component {
         return response.json();
       })
       .then(function(data) {
+        me.setState({ pastActionRows: data });
+      })
+      .catch(function(err) {
+        console.error("error while reading pastActionsInfo: ", err);
+      });
+    fetch("http://villacautela.com/actionsInfo", {
+      method: "get"
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
         me.setState({ actionRows: data });
       })
       .catch(function(err) {
         console.error("error while reading pastActionsInfo: ", err);
       });
+    fetch("http://villacautela.com/programmedActionsInfo", {
+      method: "get"
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        me.setState({ programmedActionRows: data });
+      })
+      .catch(function(err) {
+        console.error("error while reading pastActionsInfo: ", err);
+      });
+  }
+  setMenuTab(tab) {
+    console.log("new tab: " + tab);
+    this.setState({ currentTab: tab });
   }
   render() {
-    return (
-      <div>
-        <Header />
-        <Body logRows={this.state.logRows} actionRows={this.state.actionRows} />
-      </div>
-    );
+    if (this.state.currentTab === "status") {
+      return (
+        <div>
+          <Header
+            setMenuTab={tab => this.setMenuTab(tab)}
+            currentTab={this.state.currentTab}
+          />
+          <StatusBody
+            logRows={this.state.logRows}
+            actionRows={this.state.actionRows}
+            pastActionRows={this.state.pastActionRows}
+          />
+        </div>
+      );
+    } else if (this.state.currentTab === "riego") {
+      return (
+        <div>
+          <Header
+            setMenuTab={tab => this.setMenuTab(tab)}
+            currentTab={this.state.currentTab}
+          />
+          <RiegoBody
+            actionRows={this.state.actionRows}
+            programmedActionRows={this.state.programmedActionRows}
+            onProgrammedAction={i => this.modifyProgrammedValues(i)}
+            onDirectAction={i => this.modifyDirectValues(i)}
+            onDeleteProgrammedAction={i => this.deleteProgrammedValue(i)}
+          />
+        </div>
+      );
+    }
   }
 }
 
@@ -153,4 +620,20 @@ function pad(num, size) {
   var s = num + "";
   while (s.length < size) s = "0" + s;
   return s;
+}
+
+function buildGETRequest(values) {
+  let GETRequest = "";
+  let isFirst = true;
+  for (var property in values) {
+    if (values.hasOwnProperty(property)) {
+      if (isFirst) {
+        isFirst = false;
+      } else {
+        GETRequest += "&&";
+      }
+      GETRequest += property + "=" + values[property];
+    }
+  }
+  return GETRequest;
 }
